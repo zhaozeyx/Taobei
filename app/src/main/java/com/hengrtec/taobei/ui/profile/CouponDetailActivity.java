1d44b867586d25e847e5d85d9b2c8412babcbb33
zhaozeyang <zhaozeyang@zhaozeyangdeMacBook-Pro.local>
2016-06-01 18:30:58 +0800

    实现二维码展示

--- app/src/main/java/com/hengrtec/taobei/ui/profile/CouponDetailActivity.java
+++ app/src/main/java/com/hengrtec/taobei/ui/profile/CouponDetailActivity.java
@@ -13,6 +13,7 @@ package com.hengrtec.taobei.ui.profile;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -20,11 +21,17 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.hengrtec.taobei.R;
 import com.hengrtec.taobei.net.rpc.model.CouponModel;
 import com.hengrtec.taobei.ui.basic.BasicTitleBarActivity;
 import com.hengrtec.taobei.utils.DateUtils;
 import java.util.Date;
+import java.util.Hashtable;
 
 /**
  * [一句话功能简述]<BR>
@@ -85,6 +92,7 @@ public class CouponDetailActivity extends BasicTitleBarActivity {
             .FORMAT_FULL_TIME_CHINESE), DateUtils.getFormatDateTime(new Date(mCouponModel
         .getEndTime()), DateUtils.FORMAT_FULL_TIME_CHINESE), mCouponModel.getSellerName()));
     mCodeView.setText(mCouponModel.getCouponCode());
+    createQRImage(mCouponModel.getCouponCode());
   }
 
   @Override
@@ -96,12 +104,56 @@ public class CouponDetailActivity extends BasicTitleBarActivity {
     initView();
   }
 
-k  public void onClick(View view) {
+  @OnClick({R.id.btn_share_moments, R.id.btn_share_friends})
+  public void onClick(View view) {
     switch (view.getId()) {
       case R.id.btn_share_moments:
         break;
       case R.id.btn_share_friends:
         break;
+    }
+  }
+
+  private int QR_WIDTH = 200, QR_HEIGHT = 200;
+  Bitmap bitmap_zx;
+
+  public void createQRImage(String url) {
+    try {
+      //判断URL合法性
+      if (url == null || "".equals(url) || url.length() < 1) {
+        return;
+      }
+      Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+      hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+      //图像数据转换，使用了矩阵转换
+      BitMatrix bitMatrix = new QRCodeWriter().encode(url, BarcodeFormat.QR_CODE, QR_WIDTH,
+          QR_HEIGHT, hints);
+      int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
+      //下面这里按照二维码的算法，逐个生成二维码的图片，
+      //两个for循环是图片横列扫描的结果
+      for (int y = 0; y < QR_HEIGHT; y++) {
+        for (int x = 0; x < QR_WIDTH; x++) {
+          if (bitMatrix.get(x, y)) {
+            pixels[y * QR_WIDTH + x] = 0xff000000;
+          } else {
+            pixels[y * QR_WIDTH + x] = 0xffffffff;
+          }
+        }
+      }
+      //生成二维码图片的格式，使用ARGB_8888
+      bitmap_zx = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT, Bitmap.Config.ARGB_8888);
+      bitmap_zx.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
+      mQrCodeView.setImageBitmap(bitmap_zx);
+    } catch (WriterException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    if (null != bitmap_zx && !bitmap_zx.isRecycled()) {
+      bitmap_zx.recycle();
     }
   }
 
