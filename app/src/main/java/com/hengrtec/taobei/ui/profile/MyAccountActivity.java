cc377940dafb5109b664a40171bfb60c89856335
Pokesy <jiaoxianjun12@163.com>
2016-06-15 16:01:17 +0800

    我的账户UI修复调整

--- app/src/main/java/com/hengrtec/taobei/ui/profile/MyAccountActivity.java
+++ app/src/main/java/com/hengrtec/taobei/ui/profile/MyAccountActivity.java
@@ -24,57 +24,45 @@ import java.util.List;
  * Created by jiao on 2016/5/19.
  */
 public class MyAccountActivity extends BasicTitleBarActivity {
-  public static final String INTENT_ACTION_WALLET = "com.hengrtec.taobei.ui.profile" +
-      ".ACCOUNT_ACTIVITY_WALLET";
-  public static final String INTENT_ACTION_WITHDRAW = "com.hengrtec.taobei.ui.profile" +
-      ".ACCOUNT_ACTIVITY_WITHDRAW";
+  public static final String INTENT_ACTION_WALLET =
+      "com.hengrtec.taobei.ui.profile" + ".ACCOUNT_ACTIVITY_WALLET";
+  public static final String INTENT_ACTION_WITHDRAW =
+      "com.hengrtec.taobei.ui.profile" + ".ACCOUNT_ACTIVITY_WITHDRAW";
 
-  @Bind(R.id.tablayout)
-  android.support.design.widget.TabLayout tabLayout;
-  @Bind(R.id.view_pager)
-  ViewPager viewPager;
+  @Bind(R.id.tablayout) android.support.design.widget.TabLayout tabLayout;
+  @Bind(R.id.view_pager) ViewPager viewPager;
   private int[] tabIcons = {
-      R.mipmap.tab_home_normal,
-      R.mipmap.tab_mine_normal,
-      R.mipmap.tab_info_normal
+      R.mipmap.tixian, R.mipmap.wallet, R.mipmap.tab_info_normal
   };
   private int[] tabIconsPressed = {
-      R.mipmap.tab_home_passed,
-      R.mipmap.tab_mine_passed,
-      R.mipmap.tab_info_passed
+      R.mipmap.tixian_1, R.mipmap.wallet_1, R.mipmap.tab_info_passed
   };
   private List<String> titles;
   private List<Fragment> fragments;
 
-  @Override
-  protected void afterCreate(Bundle savedInstance) {
+  @Override protected void afterCreate(Bundle savedInstance) {
     ButterKnife.bind(this);
     initValue();
     initEvent();
   }
 
-  @Override
-  public boolean initializeTitleBar() {
+  @Override public boolean initializeTitleBar() {
     setMiddleTitle(R.string.myaccount);
     setLeftTitleButton(R.mipmap.icon_title_bar_back, new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
+      @Override public void onClick(View v) {
         finish();
       }
     });
     setRightTextButton(R.string.my_account_title_bar_btn_deal_records, new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
+      @Override public void onClick(View v) {
         startActivity(new Intent(MyAccountActivity.this, DealRecordsActivity.class));
       }
     });
     return true;
   }
 
-  @Override
-  public int getLayoutId() {
+  @Override public int getLayoutId() {
     return R.layout.activity_my_account;
-
   }
 
   private void initValue() {
@@ -84,8 +72,8 @@ public class MyAccountActivity extends BasicTitleBarActivity {
     titles = new ArrayList<>();
     titles.add("钱包");
     titles.add("提现");
-    FragmentViewPagerAdapter adapter = new FragmentViewPagerAdapter(getSupportFragmentManager(),
-        fragments, titles);
+    FragmentViewPagerAdapter adapter =
+        new FragmentViewPagerAdapter(getSupportFragmentManager(), fragments, titles);
     viewPager.setAdapter(adapter);
     tabLayout.setupWithViewPager(viewPager);
     setupTabIcons();
@@ -111,34 +99,31 @@ public class MyAccountActivity extends BasicTitleBarActivity {
 
   private void initEvent() {
     tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-      @Override
-      public void onTabSelected(TabLayout.Tab tab) {
+      @Override public void onTabSelected(TabLayout.Tab tab) {
         changeTabSelect(tab);
       }
 
-      @Override
-      public void onTabUnselected(TabLayout.Tab tab) {
+      @Override public void onTabUnselected(TabLayout.Tab tab) {
         changeTabNormal(tab);
       }
 
-      @Override
-      public void onTabReselected(TabLayout.Tab tab) {
+      @Override public void onTabReselected(TabLayout.Tab tab) {
 
       }
     });
-
   }
 
   private void changeTabSelect(TabLayout.Tab tab) {
     View view = tab.getCustomView();
     ImageView img_title = (ImageView) view.findViewById(R.id.img_title);
     TextView txt_title = (TextView) view.findViewById(R.id.txt_title);
-//        txt_title.setTextColor(Color.YELLOW);
+    //        txt_title.setTextColor(Color.YELLOW);
+    txt_title.setSelected(true);
     if (txt_title.getText().toString().equals("钱包")) {
-      img_title.setImageResource(R.mipmap.tab_home_passed);
+      img_title.setImageResource(R.mipmap.wallet_1);
       viewPager.setCurrentItem(0);
     } else if (txt_title.getText().toString().equals("提现")) {
-      img_title.setImageResource(R.mipmap.tab_mine_passed);
+      img_title.setImageResource(R.mipmap.tixian_1);
       viewPager.setCurrentItem(1);
     }
   }
@@ -147,21 +132,19 @@ public class MyAccountActivity extends BasicTitleBarActivity {
     View view = tab.getCustomView();
     ImageView img_title = (ImageView) view.findViewById(R.id.img_title);
     TextView txt_title = (TextView) view.findViewById(R.id.txt_title);
-//        txt_title.setTextColor(Color.WHITE);
+    //        txt_title.setTextColor(Color.WHITE);
+    txt_title.setSelected(false);
     if (txt_title.getText().toString().equals("钱包")) {
-      img_title.setImageResource(R.mipmap.tab_home_normal);
+      img_title.setImageResource(R.mipmap.wallet);
     } else if (txt_title.getText().toString().equals("提现")) {
-      img_title.setImageResource(R.mipmap.tab_mine_normal);
+      img_title.setImageResource(R.mipmap.tixian);
     }
   }
-
 
   private void setupTabIcons() {
     tabLayout.getTabAt(0).setCustomView(getTabView(0));
     tabLayout.getTabAt(1).setCustomView(getTabView(1));
-
   }
-
 
   public View getTabView(int position) {
     View view = LayoutInflater.from(this).inflate(R.layout.item_tab, null);
@@ -171,13 +154,13 @@ public class MyAccountActivity extends BasicTitleBarActivity {
     img_title.setImageResource(tabIcons[position]);
 
     if (position == 0) {
-//            txt_title.setTextColor(Color.YELLOW);
+      txt_title.setSelected(true);
       img_title.setImageResource(tabIconsPressed[position]);
     } else {
-//            txt_title.setTextColor(Color.WHITE);
+      //            txt_title.setTextColor(Color.WHITE);
+      txt_title.setSelected(false);
       img_title.setImageResource(tabIcons[position]);
     }
     return view;
   }
-
 }
